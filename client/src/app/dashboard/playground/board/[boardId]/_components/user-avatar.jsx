9adb01aba4265e5019c540278c6738614656fff1
Vansh Jain <vj2754108@gmail.com>
2026-03-30 23:04:33 +0530

    .Complete

--- client/src/app/dashboard/playground/board/[boardId]/_components/user-avatar.jsx
+++ client/src/app/dashboard/playground/board/[boardId]/_components/user-avatar.jsx
@@ -6,7 +6,7 @@ import {
   Avatar,
   AvatarFallback,
   AvatarImage,
-} from "@/components/ui/avatar";
+} from "../../../../../../Components/ui/avatar";
 import { connectionIdToColor } from "@/hooks/utils";
 
 export const UserAvatar = memo(
